5f2a6d360dfac2d58fd741907c15bc3cb90f0fe7
MahmoudYa <mahmoud.a.a.abed@gmail.com>
2024-11-27 10:09:26 +0200

    Initial commit

---
+++ src/Temp.Data/Migrations/20241119081543_AA.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Temp.Data.Migrations
+{
+    public partial class AA : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
